7cb8262608fedce940aa1d2d44223d5ca721953c
VitorFloresLeite <vitorleite260@gmail.com>
2026-05-05 19:44:48 -0300

    Criadas as classes pessoa e Cliente

---
+++ Cliente.java
@@ -0,0 +1,9 @@
+public class Cliente extends Pessoa{
+    protected double Saldo;
+
+    public Cliente(String nome, double saldo){
+        this.Nome=nome;
+        this.Saldo=saldo;
+    }
+    
+}
