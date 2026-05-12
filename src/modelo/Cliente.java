c99a17a718c0078014bbc8b2d4e190ffc05d17cb
VitorFloresLeite <vitorleite260@gmail.com>
2026-05-12 01:42:50 -0300

    Remodelagem de Cliente, correção de nomenclaturas, e adicionado perfil

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,14 +1,20 @@
+package modelo;
 public class Cliente extends Pessoa{
-    protected double saldo;
+    private double saldo;
+    private ClientePerfil perfil;
 
-    public Cliente(String nome, double saldo){
+    public Cliente(String nome, double saldo, ClientePerfil perfil){
         this.nome=nome;
         this.saldo=saldo;
+        this.perfil=perfil;
     }
-    public void SetSaldo(double saldo){
+    public void setSaldo(double saldo){
         this.saldo=saldo;
     }
-    public double GetSaldo(){
+    public double getSaldo(){
         return this.saldo;
+    }
+    public ClientePerfil getPerfil(){
+        return this.perfil;
     }
 }
