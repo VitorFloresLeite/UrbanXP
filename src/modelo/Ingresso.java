9506dfd1a39d4342dfa38c4d36ca4d1a6b185cb5
VitorFloresLeite <vitorleite260@gmail.com>
2026-05-13 00:03:26 -0300

    atualizado para suportar prioridade de acesso pelos clientes premium

--- src/modelo/Ingresso.java
+++ src/modelo/Ingresso.java
@@ -5,11 +5,13 @@ public class Ingresso {
     private Cliente cliente;
     private double valorFinalPago;
     private IngressoStatus status;
+    private boolean prioridade;
 
-    public Ingresso(Experiencia experiencia, Cliente cliente, double valorFinalPago, IngressoStatus status){
+    public Ingresso(Experiencia experiencia, Cliente cliente, double valorFinalPago, IngressoStatus status, boolean prioridade) {
         this.experiencia=experiencia;
         this.cliente=cliente;
         this.valorFinalPago=valorFinalPago;
         this.status=status;
+        this.prioridade=prioridade;
     }
 }
