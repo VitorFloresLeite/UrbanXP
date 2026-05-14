61a6e21d1e8557bea0b22670e2ec039b6b1b28b9
VitorFloresLeite <vitorleite260@gmail.com>
2026-05-13 23:21:57 -0300

    criado emtodo para exibir detalhes de ingresso e criado atributo data de emissão

--- src/modelo/Ingresso.java
+++ src/modelo/Ingresso.java
@@ -6,6 +6,7 @@ public class Ingresso {
     private double valorFinalPago;
     private IngressoStatus status;
     private boolean prioridade;
+    private String dataEmissao;
 
     public Ingresso(Experiencia experiencia, Cliente cliente, double valorFinalPago, IngressoStatus status, boolean prioridade) {
         this.experiencia=experiencia;
@@ -13,6 +14,9 @@ public class Ingresso {
         this.valorFinalPago=valorFinalPago;
         this.status=status;
         this.prioridade=prioridade;
+        this.dataEmissao = java.time.ZonedDateTime
+            .now(java.time.ZoneId.of("America/Sao_Paulo"))//fuso horário de Brasília
+            .format(java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));//formato de data e hora
     }
     public void setStatus(IngressoStatus status){//apenas o set do status, pois os outros atributos não devem ser alterados após a compra
         this.status=status;
@@ -31,5 +35,29 @@ public class Ingresso {
     }
     public boolean getPrioridade(){
         return prioridade;
+    }
+    public String getDataEmissao(){
+        return dataEmissao;
+    }
+    public void exibirDetalhes(){
+        System.out.println("-----------------------------------");
+
+        System.out.println("Evento: " + experiencia.getTitulo());
+        System.out.println("Descrição: " + experiencia.getDescricao());
+
+        System.out.println("Cliente: " + cliente.getNome());
+        System.out.println("Perfil do Cliente: " + cliente.getPerfil());
+
+        System.out.println("Custo Base: R$" + experiencia.getPrecoBase());
+        System.out.println("Valor Pago: R$" + valorFinalPago);
+        
+        System.out.println("Status: " + status);
+        System.out.println("Prioridade: " + (prioridade ? "Sim" : "Não"));
+
+        System.out.println("Data e Hora do Evento: " + experiencia.getDataHora());
+        System.out.println("Data de Emissão: " + this.getDataEmissao());
+
+        System.out.println("-----------------------------------");
+
     }
 }
