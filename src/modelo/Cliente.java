a23680c6abe7ba7ce206f14f624dc6be6326bd7b
MuriloRodrigues28 <murilomoura.aluno@unipampa.edu.br>
2026-05-14 02:14:29 -0300

    Classe "Pessoa" removida.

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,20 +1,23 @@
 package modelo;
-public class Cliente extends Pessoa{
-    private double saldo;
+public class Cliente{
+    
+    private String nome; 
+
     private ClientePerfil perfil;
+    
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+
+    public ClientePerfil getPerfil(){
+        return this.perfil;
+    }
 
     public Cliente(String nome, double saldo, ClientePerfil perfil){
         this.nome=nome;
-        this.saldo=saldo;
         this.perfil=perfil;
-    }
-    public void setSaldo(double saldo){
-        this.saldo=saldo;
-    }
-    public double getSaldo(){
-        return this.saldo;
-    }
-    public ClientePerfil getPerfil(){
-        return this.perfil;
     }
 }
